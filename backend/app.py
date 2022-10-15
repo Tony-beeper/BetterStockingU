49440f7fa3e04e851e47d78cf3b2165e2199b220
CharlesXu123 <78982364+CharlesXu123@users.noreply.github.com>
2022-10-15 14:11:20 -0400

    added requirements

--- backend/app.py
+++ backend/app.py
@@ -6,12 +6,13 @@ from dotenv import load_dotenv
 import cohere
 import training
 from flask import request
-
+import requests
 
 load_dotenv()
 COHERE = os.getenv('COHERE')
 MONGO_USER = os.getenv('MONGO_USER')
 MONGO_PASS = os.getenv('MONGO_PASS')
+TWITTER_BRARER = os.getenv('bearer_token')
 print(
     f'mongodb+srv://{MONGO_USER}:{MONGO_PASS}@cluster0.nuben.mongodb.net/?retryWrites=true&w=majority')
 client = MongoClient(
@@ -74,3 +75,22 @@ def api():
         return get_rating(company, posts)
 
     return {"hello": "world"}
+
+
+@app.route("api/twiiter/<query>")
+def search_twitter(query):
+    # api-endpoint
+    URL = "https://api.twitter.com/2/tweets/search/recent?query=lang%3Aen%20Apple&max_results=10"
+
+    headers = {"Authorization": TWITTER_BRARER}
+
+    # # defining a params dict for the parameters to be sent to the API
+    # PARAMS = {'address':location}
+
+    # sending get request and saving the response as response object
+    r = requests.get(url=URL, headers=headers)
+
+    # extracting data in json format
+    data = r.json()
+
+    print(data)
