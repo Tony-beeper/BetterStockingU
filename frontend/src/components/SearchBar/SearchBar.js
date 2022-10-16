fe1da37bec41ec5f6d7a8536afd8432396dbe3c1
CharlesXu123 <78982364+CharlesXu123@users.noreply.github.com>
2022-10-15 23:30:19 -0400

    edit search page

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,22 +1,64 @@
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import Button from '@mui/material/Button';
-import './SearchBar.css'
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./SearchBar.css";
 
+const SearchBar = ({ options }) => {
+  const [topic, setTopic] = useState("");
+  const [topics, setTopics] = useState([]);
 
-const SearchBar = ({options }) => {
+  useEffect(async () => {
+    console.log(topics);
+  }, [topics]);
+
   return (
-    <div className='search-bar'>
-    <Autocomplete
-  disablePortal
-  id="combo-box-demo"
-  options={options}
-  sx={{ width: 300 }}
-  renderInput={(params) => <TextField {...params} label="Search" />}
-  
-/>
-<Button variant="text" style={{display:"block"}}>Add</Button>
-</div>
+    <div className="search-bar">
+      <Box mb={1}>
+        <Stack direction="row" spacing={1}>
+          {topics.map((value, idx) => (
+            <Chip label={value} idx={idx} variant="outlined" />
+          ))}
+        </Stack>
+      </Box>
+
+      <div className="search-area">
+        <TextField
+          id="outlined-basic"
+          label="Search Topic"
+          variant="outlined"
+          value={topic}
+          sx={{ marginRight: 1 }}
+          onChange={(e) => setTopic(e.target.value)}
+        />
+
+        <Button
+          variant="contained"
+          sx={{ marginRight: 1 }}
+          style={{ display: "block" }}
+          onClick={(e) => {
+            if (topic !== "") {
+              setTopics([...topics, topic]);
+              setTopic("");
+            }
+          }}
+        >
+          Add
+        </Button>
+        <Button
+          variant="contained"
+          style={{ display: "block" }}
+          onClick={(e) => {
+            setTopics([]);
+          }}
+        >
+          clear
+        </Button>
+      </div>
+    </div>
   );
 };
 export default SearchBar;
