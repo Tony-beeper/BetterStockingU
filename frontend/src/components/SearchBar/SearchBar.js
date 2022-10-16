dab119f2dff8e5822f52d288997df07d6e321145
Colin Lin <colincool100@gmail.com>
2022-10-15 20:56:34 -0400

    search styling

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,21 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+import './SearchBar.css'
+
 
 const SearchBar = ({options }) => {
   return (
-    <div>
+    <div className='search-bar'>
     <Autocomplete
   disablePortal
   id="combo-box-demo"
   options={options}
   sx={{ width: 300 }}
-  renderInput={(params) => <TextField {...params} label="Twitter Username" />}
+  renderInput={(params) => <TextField {...params} label="Search" />}
   
 />
+<Button variant="text" style={{display:"block"}}>Add</Button>
 </div>
   );
 };
